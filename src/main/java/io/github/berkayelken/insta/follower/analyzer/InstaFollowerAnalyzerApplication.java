a5bdc31718eef2412cdeb4ad73c40e05e04849c3
Berkay Yelken <berkayelken@gmail.com>
2025-04-22 13:27:41 +0300

    repacking

--- src/main/java/io/github/berkayelken/insta/follower/analyzer/InstaFollowerAnalyzerApplication.java
+++ src/main/java/io/github/berkayelken/insta/follower/analyzer/InstaFollowerAnalyzerApplication.java
@@ -1,4 +1,4 @@
-package com.example.instafolloweranalyzer;
+package io.github.berkayelken.insta.follower.analyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
