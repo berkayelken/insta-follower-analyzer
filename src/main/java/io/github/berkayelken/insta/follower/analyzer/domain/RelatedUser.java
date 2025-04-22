e633f38192a73ef8695b7bb6097498ea90d1ccba
Berkay Yelken <berkayelken@gmail.com>
2025-04-22 13:55:27 +0300

    related user equality implementation

--- src/main/java/io/github/berkayelken/insta/follower/analyzer/domain/RelatedUser.java
+++ src/main/java/io/github/berkayelken/insta/follower/analyzer/domain/RelatedUser.java
@@ -5,10 +5,29 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 @ToString
 public class RelatedUser {
 	@JsonProperty("string_list_data")
 	private UserInfo userInfo;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		if(userInfo == null)
+			return false;
+		RelatedUser that = (RelatedUser) o;
+		return userInfo.equals(that.userInfo);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(userInfo);
+	}
 }
--- src/main/java/io/github/berkayelken/insta/follower/analyzer/domain/UserInfo.java
+++ src/main/java/io/github/berkayelken/insta/follower/analyzer/domain/UserInfo.java
@@ -3,6 +3,9 @@ package io.github.berkayelken.insta.follower.analyzer.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.util.StringUtils;
+
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -11,4 +14,21 @@ public class UserInfo {
 	private String href;
 	private String value;
 	private long timestamp;
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		if(!StringUtils.hasText(value))
+			return false;
+		UserInfo userInfo = (UserInfo) o;
+		return value.equalsIgnoreCase(userInfo.value);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(value);
+	}
 }
